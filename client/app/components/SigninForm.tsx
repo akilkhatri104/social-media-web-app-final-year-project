4b90889ff4a287dba693db071f0ffb885dbf8a08
Akil Khatri <khatriakil104@gmail.com>
2026-06-25 10:27:47 +0530

    Add .env.sample file and remove unnecessary dev flags from package-lock.json
    - Created a sample environment configuration file (.env.sample) with placeholders for essential variables such as PORT, FRONTEND_URL, BACKEND_URL, DATABASE_URL, Cloudinary credentials, and SMTP settings.
    - Cleaned up package-lock.json by removing redundant "dev" flags from various dependencies to streamline the file and improve clarity.

--- client/app/components/SigninForm.tsx
+++ client/app/components/SigninForm.tsx
@@ -22,13 +22,9 @@ console.log("Backend URL:", import.meta.env.VITE_BACKEND_URL);
 
 const formSchema = z.object({
     username: z
-        .string()
-        .min(3, "Username must be at least 3 characters.")
-        .max(32, "Username must be at most 32 characters."),
+        .string(),
     password: z
         .string()
-        .min(8, "Password must be at least 8 characters.")
-        .max(128, "Password must be at most 128 characters."),
 })
 
 export const SigninForm = () => {
